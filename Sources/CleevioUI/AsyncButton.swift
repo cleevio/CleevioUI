28f5b6fecf5b326edb0937b2e797b46ba49c37aa
Lukáš Valenta <valenta@lvalenta.cz>
2023-08-23 15:27:15 +0200

    Documentation

--- Sources/CleevioUI/AsyncButton.swift
+++ Sources/CleevioUI/AsyncButton.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+/// A button that performs an asynchronous action when tapped that guards that the action is run maximally once at the same time.
 public struct AsyncButton<Label: View>: View {
+    /// The asynchronous action to perform when the button is tapped.
     public var action: () async -> Void
+    
+    /// The label of the button.
     public var label: Label
+    
+    /// Internal state to track whether the action is currently executing a task.
     @State private var isExecuting = false
 
+    /// Creates an asynchronous button with the given action and label.
+    /// - Parameters:
+    ///   - action: The asynchronous action to perform when the button is tapped.
+    ///   - label: A closure returning the label of the button.
     @inlinable
     public init(action: @escaping () async -> Void,
                 label: () -> Label) {
@@ -36,6 +46,10 @@ public struct AsyncButton<Label: View>: View {
 }
 
 extension AsyncButton where Label == Text {
+    /// Creates an asynchronous button with the given title and action.
+    /// - Parameters:
+    ///   - title: The title of the button.
+    ///   - action: The asynchronous action to perform when the button is tapped.
     @inlinable
     public init(_ title: some StringProtocol, action: @escaping () async -> Void) {
         self.init(action: action) { Text(title) }
