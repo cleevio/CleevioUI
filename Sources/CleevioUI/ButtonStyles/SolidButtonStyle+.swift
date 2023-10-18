d545526b66f8535117d035ccb71c0a1f3f1a02d6
Vendula Švastalová <vendula.svastalova@cleevio.cz>
2023-10-18 22:38:27 +0200

    Replace EmptyView with LoadingView

--- Sources/CleevioUI/ButtonStyles/SolidButtonStyle+.swift
+++ Sources/CleevioUI/ButtonStyles/SolidButtonStyle+.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-public extension SolidButtonStyle where Background == Color, Overlay == RoundedStroke, LoadingView == EmptyView {
+public extension SolidButtonStyle where Background == Color, Overlay == RoundedStroke, LoadingView == LoadingView {
     init(
         foregroundColorSet: ButtonStateColorSet,
         backgroundColorSet: ButtonStateColorSet,
